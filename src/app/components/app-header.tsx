af81d8282a85b808faf005748ded9965757b9b52
Umesh Basnet <basnet0umesh@gmail.com>
2025-07-22 11:35:20 -0400

    - [x] fix project add

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -21,7 +21,7 @@ export default function AppHeader({ onNewProjectClick, shouldShowNewProjectButto
       />
       <div className="flex items-center gap-4">
 
-        {shouldShowNewProjectButton && <button
+        {onNewProjectClick && <button
           onClick={onNewProjectClick}
           className="px-4 py-2 bg-blue-600 text-white text-sm rounded hover:bg-blue-700 transition-colors"
         >
